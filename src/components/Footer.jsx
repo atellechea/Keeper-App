1419695017e9e97cdad6f3a5f769b2f254378515
Adrian <atellechearascon@gmail.com>
2020-08-27 11:29:51 -0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+
+    const currentYear = new Date().getFullYear();
+    return <footer>
+        <p>Copy right {currentYear}</p>
+    </footer>
+    
+}
+
+export default Footer;
