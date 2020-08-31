770207dfaedc2b1296bec38a9f2342bb0919111e
Adrian <atellechearascon@gmail.com>
2020-08-31 16:42:03 -0700

    Add props and arrow function to our notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import Notes from "../notes";
 
 function App(){
     return <div>
         <Header/>
         <Footer/>
-        <Note />
+        {Notes.map((note) => 
+        <Note 
+            key = {note.id}
+            title={note.title}
+            content={note.content}
+        />
+)}
+        {/* <Note title="" content="Contenido"/> */}
     </div>
 }
 
